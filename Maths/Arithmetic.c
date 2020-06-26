730d37e2c9615a68bae1acc4a053dd7b59670b9d
Jai Shri Ram <namanchaurasia1208@gmail.com>
2020-06-26 15:26:24 +0530

    merged 2 arithmetics

--- Maths/Arithmetic.c
+++ Maths/Arithmetic.c
@@ -18,4 +18,12 @@ int main()
       printf("%d",a+b);
       return 0;
 }
-
+simple function
+{
+	int a=10,b=20,c;
+	printf("%d",a+b);
+	return 0;
+}
+implement factorial and others
+do it nicely
+on remote master
